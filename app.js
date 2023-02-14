c84e28f20bd4274cfff7ec344715981b162c0164
Shelby Benton <shelby.benton@cru.org>
2023-02-13 19:32:16 -0500

    final touches

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
+  const releaseDay = 14
   const timeReleases = {
     // Good Morning
-    modal1: { reveal_hour: 0, answer: "true", reward: "address"},
+    modal1: { revealHour: 0},
     // coffee
-    modal2: { reveal_hour: 9, answer: "true", reward: "address"},
+    modal2: { revealHour: 9},
     // dinner
-    modal3: { reveal_hour: 16, answer: "true", reward: "address"},
+    modal3: { revealHour: 16},
     // dessert
-    modal4: { reveal_hour: 17, answer: "true", reward: "address"},
+    modal4: { revealHour: 17},
     // homeward
-    modal5: { reveal_hour: 20, answer: "true", reward: "address"},
+    modal5: { revealHour: 20},
     // next day
-    modal6: { reveal_hour: 23, answer: "true", reward: "address"}
+    modal6: { revealHour: 24}
   }
 
   function setTime() {
@@ -22,8 +23,8 @@ $(document).ready(function(){
     var min = today.getMinutes();
     time = hr+':'+min;
     // !!!!!!!!! Update dd == 14 !!!!!!!!!!!
-    if (dd >= 14 && mm == 2) {
-      $(".alert").text("It's Valentines Day! The time is " + time + ".")
+    if (dd >= releaseDay && mm == 2) {
+      $(".alert").text("It's Valentines Day! Are you ready for the nerdiest Valentine card ever. I know you have the skillz to hack this, so no cheating!")
       unveil(dd, hr)
     }
   }
@@ -31,11 +32,10 @@ $(document).ready(function(){
   function unveil(dd, hr){
     $(".unveil_modal").each(function(){
       selected_modal = this.id
-      release_time = timeReleases[selected_modal].reveal_hour
-      if (dd == 13, hr >= release_time){
+      release_time = timeReleases[selected_modal].revealHour
+      if (dd == releaseDay, hr >= release_time){
         this.style.display = "block"
-      }
-      else if (dd > 14) {
+      } else if (dd > releaseDay) {
         this.style.display = "block"
       }
     })
